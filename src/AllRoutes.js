215e3bf7b225187bac44424bb6574736eb2a281d
Anastasija Pankova <anastasija.pankova@yahoo.com>
2023-05-20 17:10:36 +0200

    post page

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import ResultsPage from "./components/ResultsPage";
 import { Main } from "./components/main";
+import { Post } from "./components/post"
 import LoginPage from "./components/LoginPage";
 import UserPage from "./components/UserPage";
 import AllUsersPage from "./components/AllUsersPage";
@@ -10,11 +11,11 @@ function AllRoutes() {
         <Routes>
             <Route exact path="/" element={<Main />} />
             <Route path="/results" element={<ResultsPage />} />
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/all-users" element={<AllUsersPage/>}/>
-            <Route path="/user/:id" element={<UserPage/>}/>
-            <Route path="post/:id"/>
-            <Route path="*" element={<h2>Error 404: Page Not Found</h2>}/>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/all-users" element={<AllUsersPage />} />
+            <Route path="/user/:id" element={<UserPage />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="*" element={<h2>Error 404: Page Not Found</h2>} />
         </Routes>
     );
 }
